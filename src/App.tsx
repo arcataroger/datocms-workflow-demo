60025eb14a596e30e00498ebe43686d7115d7e35
arcataroger <arcataroger@gmail.com>
2024-05-30 17:47:53 -0700

    Basic mutation

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,12 @@ import {
     useQuery,
     useMutation,
     useQueryClient,
-    QueryClient,
-    QueryClientProvider,
 } from '@tanstack/react-query'
 
 
 function App() {
     registerLicense(import.meta.env.VITE_LICENSE_KEY);
+    const queryClient = useQueryClient();
     const datoClient = buildClient({apiToken: import.meta.env.VITE_OWNER_TOKEN});
     const workflowId = 'Kq5bp4ZFQQC-COCo13qhMQ'
     const workflowsQuery = useQuery({
@@ -22,77 +21,32 @@ function App() {
     })
 
     const featuresQuery = useQuery({
-        queryKey: ['cAey7LErScKx4pTRFRKTkw'],
+        queryKey: ['feature'],
         queryFn: async () => await datoClient.items.list({
             filter: {
                 type: 'feature'
             }
-        })
+        }),
+        refetchInterval: 1000
     })
 
-
-    const data = [
-        {
-            Id: 1,
-            Status: 'Open',
-            Summary: 'Analyze the new requirements gathered from the customer.',
-            Type: 'Story',
-            Priority: 'Low',
-            Tags: 'Analyze,Customer',
-            Estimate: 3.5,
-            Assignee: 'Nancy Davloio',
-            RankId: 1
+    const featureMutator = useMutation({
+        mutationFn: async (feature: Item) => {
+            let featureWithoutStage = feature
+            featureWithoutStage.meta.stage = featureWithoutStage.stage
+            delete featureWithoutStage.stage
+            await datoClient.items.update(feature.id, featureWithoutStage)
         },
-        {
-            Id: 2,
-            Status: 'InProgress',
-            Summary: 'Fix the issues reported in the IE browser.',
-            Type: 'Bug',
-            Priority: 'Release Breaker',
-            Tags: 'IE',
-            Estimate: 2.5,
-            Assignee: 'Janet Leverling',
-            RankId: 2
+        onSuccess: () => {
+            // Invalidate and refetch
+            queryClient.invalidateQueries({ queryKey: ['feature'] })
         },
-        {
-            Id: 3,
-            Status: 'Testing',
-            Summary: 'Fix the issues reported by the customer.',
-            Type: 'Bug',
-            Priority: 'Low',
-            Tags: 'Customer',
-            Estimate: '3.5',
-            Assignee: 'Steven walker',
-            RankId: 1
-        },
-        {
-            Id: 4,
-            Status: 'Close',
-            Summary: 'Arrange a web meeting with the customer to get the login page requirements.',
-            Type: 'Others',
-            Priority: 'Low',
-            Tags: 'Meeting',
-            Estimate: 2,
-            Assignee: 'Michael Suyama',
-            RankId: 1
-        },
-        {
-            Id: 5,
-            Status: 'Validate',
-            Summary: 'Validate new requirements',
-            Type: 'Improvement',
-            Priority: 'Low',
-            Tags: 'Validation',
-            Estimate: 1.5,
-            Assignee: 'Robert King',
-            RankId: 1
-        }
-    ]
+    })
 
     return (
         <div className="App">
             <h1>DatoCMS Workflow Demo</h1>
-            <KanbanComponent id="kanban" keyField="stage" dataSource={featuresQuery?.data?.map((feature) => ({
+            <KanbanComponent id="kanban" keyField="stage" dragStop={feature => featureMutator.mutate(feature.data[0])} dataSource={featuresQuery?.data?.map((feature) => ({
                 ...feature,
                 stage: feature.meta?.stage
             }))}
