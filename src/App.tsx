23a5cb678fdc53017a6f09f2674a9a806415768b
arcataroger <arcataroger@gmail.com>
2024-05-30 18:01:02 -0700

    Simpler updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import {KanbanComponent, ColumnsDirective, ColumnDirective} from "@syncfusion/ej
 import './App.css';
 import {registerLicense} from '@syncfusion/ej2-base'
 import {buildClient} from "@datocms/cma-client-browser";
-import {Workflow, Item} from "@datocms/cma-client/dist/types/generated/SimpleSchemaTypes";
+import {Item} from "@datocms/cma-client/dist/types/generated/SimpleSchemaTypes";
 import {
     useQuery,
     useMutation,
@@ -32,34 +32,33 @@ function App() {
 
     const featureMutator = useMutation({
         mutationFn: async (feature: Item) => {
-            let featureWithoutStage = feature
-            featureWithoutStage.meta.stage = featureWithoutStage.stage
-            delete featureWithoutStage.stage
-            await datoClient.items.update(feature.id, featureWithoutStage)
+            await datoClient.items.update(feature.id, {
+                meta: {
+                    stage: feature.stage as string // Not feature.meta.stage! The Kanban component uses the top-level stage that we cloned
+                }
+            })
         },
         onSuccess: () => {
             // Invalidate and refetch
-            queryClient.invalidateQueries({ queryKey: ['feature'] })
+            queryClient.invalidateQueries({queryKey: ['feature']})
         },
     })
 
     return (
         <div className="App">
             <h1>DatoCMS Workflow Demo</h1>
-            <KanbanComponent id="kanban" keyField="stage" dragStop={feature => featureMutator.mutate(feature.data[0])} dataSource={featuresQuery?.data?.map((feature) => ({
-                ...feature,
-                stage: feature.meta?.stage
-            }))}
+            <KanbanComponent id="kanban" keyField="stage" dragStop={feature => featureMutator.mutate(feature.data[0])}
+                             dataSource={featuresQuery?.data?.map((feature) => ({
+                                 ...feature,
+                                 stage: feature.meta?.stage // TODO figure out if there's a way to just tell this kanban component to use meta.stage as the keyField
+                             }))}
                              cardSettings={{contentField: "description", headerField: "name"}}>
                 <ColumnsDirective>
-                    {workflowsQuery?.data?.stages?.map(stage => <ColumnDirective headerText={stage.name} keyField={stage.id} />)}
+                    {workflowsQuery?.data?.stages?.map(stage => <ColumnDirective headerText={stage.name}
+                                                                                 keyField={stage.id}/>)}
                 </ColumnsDirective>
             </KanbanComponent>
 
-            <h3>Workflow</h3>
-            <pre>
-                {JSON.stringify(workflowsQuery.data, null, 2)}
-            </pre>
             <h3>Records</h3>
             <pre>
                 {JSON.stringify(featuresQuery.data, null, 2)}
